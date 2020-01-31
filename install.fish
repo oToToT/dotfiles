3eee66c692aa3b398d3419bf1e795de17dc9dc2a
oToToT <ty1208chiang@gmail.com>
2020-01-31 17:10:57 +0800

    update fish installation

--- install.fish
+++ install.fish
@@ -1,13 +1,15 @@
 #!/usr/bin/fish
-#
+
 # install fish
-curl -L https://get.oh-my.fish | fish
-ln -s "$HOME/dotfiles/config/" "$HOME/.config/"
+curl -L https://get.oh-my.fish > install
+fish install --path=~/.local/share/omf --config=~/.config/omf --yes --noninteractive
+rm install
+ln -s "$HOME/dotfiles/config" "$HOME/.config"
 # setup tmux
 ln -s "$HOME/dotfiles/tmux.conf" "$HOME/tmux.conf"
 # install gdb stuff
-ln -s "$HOME/dotfiles/Pwngdb/" "$HOME/pwngdb/"
-ln -s "$HOME/dotfiles/peda/" "$HOME/peda/"
+ln -s "$HOME/dotfiles/Pwngdb" "$HOME/pwngdb"
+ln -s "$HOME/dotfiles/peda" "$HOME/peda"
 ln -s "$HOME/dotfiles/gdbinit" "$HOME/.gdbinit"
 # install git stuff
 ln -s "$HOME/dotfiles/gitconfig" "$HOME/.gitconfig"
