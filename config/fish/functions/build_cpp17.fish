dd049a931649dce8d94713cb24d963e9214df9b1
oToToT <ty1208chiang@gmail.com>
2021-02-01 19:42:39 +0800

    update compile arguments

--- config/fish/functions/build_cpp17.fish
+++ config/fish/functions/build_cpp17.fish
@@ -1,3 +1,3 @@
 function build_cpp17
-    g++ $argv[1].cpp -o $argv[1].exe -std=c++17 -Wall -Wextra -Wconversion -Wshadow -fsanitize=leak -fsanitize=undefined -fsanitize=address -g
+    g++ $argv[1].cpp -o $argv[1].exe -std=c++17 -Wall -Wextra -Wconversion -Wshadow -Wwrite-strings -fsanitize=leak -fsanitize=undefined -fsanitize=address -g
 end
