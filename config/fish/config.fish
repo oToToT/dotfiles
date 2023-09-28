4cc4936afe9f8c32108799a70a92fb9f55815867
Tommy Chiang <ty1208chiang@gmail.com>
2023-09-29 00:58:24 +0800

    for wsl2

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,5 @@
 set -gx VISUAL vim
 set -gx EDITOR "$VISUAL"
-set -gx DISPLAY 127.0.0.1:0
-set -gx XDG_SESSION_TYPE x11
 set -gx PULSE_SERVER tcp:127.0.0.1
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
