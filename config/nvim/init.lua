234f3c3163b3d1bd19dfbb6702f528c482f6e0f6
Tommy Chiang <ototot@google.com>
2025-09-22 22:24:40 +0800

    [nvim] Update packages automatically

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,4 @@
 require("config.options")
 require("config.keymaps")
 require("config.lazy")
+vim.api.nvim_create_autocmd("VimEnter", { callback = function() require "lazy".update({ show = false }) end })
