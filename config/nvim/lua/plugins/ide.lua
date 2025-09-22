fdda2dc5781902bc3bf6242630071f0837931e2e
Tommy Chiang <ototot@google.com>
2025-09-22 22:36:11 +0800

    [nvim] Add indent lines

--- config/nvim/lua/plugins/ide.lua
+++ config/nvim/lua/plugins/ide.lua
@@ -94,4 +94,21 @@ return {
       }
     },
   },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      indent = {
+        char = "⎸",
+      },
+      scope = {
+        enabled = true,
+        show_start = false,
+        show_end = false,
+      },
+      exclude = {
+        filetypes = { "help", "dashboard", "packer", },
+      },
+    }
+  }
 }
