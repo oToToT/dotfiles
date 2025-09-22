9fde0b3cca7808f7f8625cf03452a3c42a2ea252
Tommy Chiang <ototot@google.com>
2025-09-22 22:31:48 +0800

    [nvim] Add conform.nvim to format-on-save

--- config/nvim/lua/plugins/ide.lua
+++ config/nvim/lua/plugins/ide.lua
@@ -62,4 +62,19 @@ return {
       require("guess-indent").setup {}
     end
   },
+  {
+    'stevearc/conform.nvim',
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          kotlin = { "ktfmt" },
+          javascriptreact = { "prettierd" },
+        },
+        format_on_save = {
+          timeout_ms = 10000,
+          lsp_format = "fallback",
+        }
+      })
+    end,
+  },
 }
