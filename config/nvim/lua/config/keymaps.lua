f53e1fd7737b911b23b86f87ea893d3d65d40fb0
Tommy Chiang <ototot@google.com>
2026-04-06 18:07:01 +0800

    [nvim] Update Python LSP to ty

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -4,6 +4,7 @@ vim.api.nvim_set_keymap("v", "<Leader>c<Space>", "gc", {})
 vim.api.nvim_set_keymap("n", "<Leader>w", "<Cmd>w!<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Leader>p", "<Cmd>set invpaste<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Leader>d", "<Cmd>let @/=''<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>xx", "<Cmd>:cclose<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<space>e", "<Cmd>lua vim.diagnostic.open_float()<CR>", {})
 
 vim.api.nvim_create_user_command("ToggleConform", function()
--- config/nvim/lua/plugins/basics.lua
+++ config/nvim/lua/plugins/basics.lua
@@ -2,7 +2,7 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = { 'pylsp', 'clangd', 'superhtml', 'lua_ls', 'kotlin_lsp', 'jsonls', 'rust_analyzer', 'cssls' },
+      ensure_installed = { 'ty', 'clangd', 'superhtml', 'lua_ls', 'kotlin_lsp', 'jsonls', 'rust_analyzer', 'cssls' },
     },
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
@@ -10,6 +10,15 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
           local opts = { noremap = true, silent = true }
+          local python_root_markers = {
+            'ty.toml',
+            'pyproject.toml',
+            'setup.py',
+            'setup.cfg',
+            'requirements.txt',
+            '.git',
+          }
+
           -- Use an on_attach function to only map the following keys
           -- after the language server attaches to the current buffer
           local on_attach = function(client, bufnr)
@@ -37,13 +46,53 @@ return {
 
           -- Use a loop to conveniently call 'setup' on multiple servers and
           -- map buffer local keybindings when the language server attaches
-          local servers = { 'pylsp', 'clangd', 'superhtml', 'lua_ls', 'kotlin_lsp', 'jsonls', 'cssls' }
+          local servers = { 'clangd', 'superhtml', 'lua_ls', 'kotlin_lsp', 'jsonls', 'cssls' }
           for _, lsp in pairs(servers) do
             vim.lsp.config(lsp, {
               on_attach = on_attach,
             })
             vim.lsp.enable(lsp)
           end
+
+          vim.lsp.config('ty', {
+            on_attach = on_attach,
+          })
+
+          local ty_augroup = vim.api.nvim_create_augroup('ty_python_lsp', { clear = true })
+          vim.api.nvim_create_autocmd('FileType', {
+            group = ty_augroup,
+            pattern = 'python',
+            callback = function(args)
+              local bufnr = args.buf
+              local filepath = vim.api.nvim_buf_get_name(bufnr)
+              if filepath == '' then
+                return
+              end
+
+              local first_line = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ''
+              local has_inline_metadata = first_line:match('^# /// script')
+
+              local name = 'ty'
+              local cmd = { 'uvx', 'ty', 'server' }
+              local root_dir = vim.fs.root(filepath, python_root_markers) or vim.fs.dirname(filepath)
+
+              if has_inline_metadata then
+                local filename = vim.fn.fnamemodify(filepath, ':t')
+                local relpath = vim.fn.fnamemodify(filepath, ':.')
+
+                name = 'ty-' .. filename
+                cmd = { 'uvx', '--with-requirements', relpath, 'ty', 'server' }
+                root_dir = vim.fs.dirname(filepath)
+              end
+
+              vim.lsp.start({
+                name = name,
+                cmd = cmd,
+                root_dir = root_dir,
+                bufnr = bufnr,
+              })
+            end,
+          })
 
           -- custom settngs for rust-analyzer
           vim.lsp.config('rust_analyzer', {
